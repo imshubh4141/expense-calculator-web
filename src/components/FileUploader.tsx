fe4e86792778ff3b07fbe6e1108136537bb84ea1
imshubh4141 <shubhchikku@gmail.com>
2023-08-13 18:29:41 +0530

    added FileUploader component

---
+++ src/components/FileUploader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function FileUploader() {
+    return (
+        <div className=''>This is where you will upload a file</div>
+    );
+}
+
+export default FileUploader;
+
