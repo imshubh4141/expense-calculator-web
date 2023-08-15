19c1a3bf243dffa996f7638eb0e2b6b92b2d229b
imshubh4141 <shubhchikku@gmail.com>
2023-08-15 20:43:01 +0530

    added FileUploader component

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import '../App.css';
 
 function FileUploader() {
+
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0]//safe way to access arrays
+        setFile(selectedFile || null);
+    }
+
     return (
-        <div className=''>This is where you will upload a file</div>
+        <div className='File-uploader'>
+            <form action="GET">
+                <h4>Upload excel file</h4>
+                <input type="file" onChange={handleChange}/>
+                <button type='submit'>Calculate my expenses</button>
+            </form>
+        </div>
     );
 }
 
