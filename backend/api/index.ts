349b39af2b3fe7d5269ee49e6f21e9f6e793a845
Shubh Gupta <shubh@Shubhs-MacBook-Air.local>
2024-04-13 01:04:00 +0530

    commented dev env code

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -53,7 +53,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build'))); //--> for prod
-app.use(express.static('/Users/shubh/Desktop/repos/expense-calculator-web/build')); //--> for dev
+// app.use(express.static('/Users/shubh/Desktop/repos/expense-calculator-web/build')); //--> for dev
 
 
 app.get('/', (req: Request, res: Response) => {
